f2cbd5d6f0fa8e21643c9c873b3186bef21e9de9
Ng.V.Hoàng <134377540+NeverYeeu@users.noreply.github.com>
2023-10-30 11:08:52 +0700

    upcode

---
+++ index.js
@@ -0,0 +1,19 @@
+const urlApi = "https://jsonplaceholder.typicode.com/users"
+fetch(urlApi) 
+	.then (res => res.json())
+	.then ((users) =>
+		$('#users').innerHTML = users.map((item) => {
+			let {name, username} = item
+			return (`
+				<div class="infor">
+					<span>${name}</span>
+					<div>${username}</div>
+				</div>
+			`)
+		}).join('')
+
+	)
+	.catch( () => {
+		console.log('Không thể gọi được API')
+	})
+const $ = document.querySelector.bind(document);
