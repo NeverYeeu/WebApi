b22d2f4ca14b4750fec92b64b4035f60c7720e7d
Ng.V.Hoàng <134377540+NeverYeeu@users.noreply.github.com>
2023-11-10 15:23:32 +0700

    them js - render ra giao dien

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const $ = document.querySelector.bind(document)
-const urlApi = "http://localhost:3000/users";
+const urlApi = "http://localhost:3000/manhua";
 const button = $('#button');
 function startWeb(){
 	getApi(renderData);
@@ -36,17 +36,31 @@ function deleteApi(id){
 		.then(res => res.json())
 		.then(() => {
 			let item = $('.item-' + id);
-			item.remove();
+			let isChoose = confirm('Bạn có muốn xóa truyện này không?');
+			if(isChoose == true) {
+				item.remove();
+				console.log('xóa truyện')
+			} else{
+				console.log('Không xóa truyện')
+			}
 		})
 }
 function renderData(array) {
 	$('#users').innerHTML = array.map(
 		(item) => {
-			let {name, username, id} = item
+			let {nameComic, mainComic,imageComic,chapComic, genreComic, id} = item
 			return (`
 				<div class="infor item-${id}">
-					<span>Name: ${name}</span>
-					<div>User name: ${username}</div>
+					<div class="wrap-infor">
+						<img class="imageComic" src=${imageComic}>
+						<input class="chapComic" value=${chapComic}>
+					</div>
+					<div class="comic-infor">
+						<span>Name: ${nameComic}</span>
+						<div>User name: ${mainComic}</div> 
+						<input class="genreComic" value=${genreComic}>
+						<input class="imageComic2" value=${imageComic}>
+					</div>
 					<div class="delete-btn" onclick="deleteApi(${id})">Xóa</div>
 				</div>
 			`)
@@ -57,11 +71,17 @@ function renderData(array) {
 //Xử lý khi ta nhập dữ liệu------------------------------------------------------------------
 function handleForm(){
 	button.addEventListener('click', () => {
-		let name = $('input[name="name"]').value;
-		let username = $('input[name="userName"]').value
+		let nameComic = $('input[name="name"]').value;
+		let mainComic = $('input[name="userName"]').value;
+		let imageComic = $('input[name="imageComic"]').value;
+		let chapComic = $('input[name="chapComic"]').value;
+		let genreComic = $('input[name="genreComic"]').value;
 		var formData = {
-			name: name,
-			username: username
+			nameComic: nameComic,
+			mainComic: mainComic,
+			imageComic: imageComic,
+			chapComic: chapComic,
+			genreComic: genreComic
 		}
 		postApi(formData, () => {
 			getApi(renderData);
